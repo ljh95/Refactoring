e767012eb5c612967927d37e2f089651c505883c
ljh95 <ilegwmam@gmail.com>
2021-12-08 20:47:56 +0900

    변순 인라인 하기, 함수 선언 바꾸기

--- chapter01/state함수쪼개기.js
+++ chapter01/state함수쪼개기.js
@@ -30,8 +30,8 @@ const plays = {
     "type": "tragedy"
   }
 };
-
 function statement(invoice, plays){
+
   let totalAmount = 0;
   let volumeCredits = 0;
   let result = `청구 내역 (고객명: ${invoice.customer})\n`;
@@ -42,47 +42,53 @@ function statement(invoice, plays){
   }).format;
 
   for(let perf of invoice.performances) {
-    const play = plays[perf.playID];
-    let thisAmount = amountFor(perf, play);
+    // const play = playFor(perf); // 우변을 함수로 추출
+    let thisAmount = amountFor(perf, playFor(perf));
 
     // 포인트를 적립한다.
     volumeCredits += Math.max(perf.audience - 30, 0);
     // 희극 관객 5명마다 추가 포인트를 제공한다.
-    if("comedy" === play.type) volumeCredits += Math.floor(perf.audience / 5);
+    if("comedy" === playFor(perf).type) volumeCredits += Math.floor(perf.audience / 5);
 
     // 청구 내역을 출력한다.
-    result += `  ${play.name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
+    result += `  ${playFor(perf).name}: ${format(thisAmount / 100)} (${perf.audience}석)\n`;
     totalAmount += thisAmount;
   }
 
   result += `총액: ${format(totalAmount/100)}\n`;
   result += `적립 포인트: ${volumeCredits}점\n`;
   return result;
-}
-
-function amountFor(perf, play) { // 값이 바뀌지 않는 변수는 매개변수로 전달
-  let thisAmount = 0; // 변수를 초기화하는 코드
-
-  switch (play.type) {
-    case "tragedy": // 비극
-      thisAmount = 40000;
-      if(perf.audience > 30) {
-        thisAmount += 1000 * (perf.audience - 30);
-      }
-      break;
-    case "comedy": // 희극
-      thisAmount = 30000;
-      if(perf.audience > 20) {
-        thisAmount += 10000 + 500 * (perf.audience - 20);
-      }
-      thisAmount += 300 * perf.audience;
-      break;
-    default:
-      throw new Error(`알 수 없는 장르: ${play.type}`);
+  
+  function amountFor(aPerformance) { // 값이 바뀌지 않는 변수는 매개변수로 전달
+    let result = 0; // 함수의 반환값에는 항상 resultfk gksek.
+  
+    switch (playFor(aPerformance).type) {
+      case "tragedy": // 비극
+        result = 40000;
+        if(aPerformance.audience > 30) {
+          result += 1000 * (aPerformance.audience - 30);
+        }
+        break;
+      case "comedy": // 희극
+        result = 30000;
+        if(aPerformance.audience > 20) {
+          result += 10000 + 500 * (aPerformance.audience - 20);
+        }
+        result += 300 * aPerformance.audience;
+        break;
+      default:
+        throw new Error(`알 수 없는 장르: ${playFor(aPerformance).type}`);
+    }
+  
+    return result; // 함수 안에서 값이 바뀌는 변수 반환
   }
 
-  return thisAmount; // 함수 안에서 값이 바뀌는 변수 반환
+  function playFor(aPerformance) {
+    return plays[aPerformance.playID];
+  }
+
 }
+
 
 
 console.log(statement(invoices, plays));
