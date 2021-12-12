a4b944dc59d3e4d9174c3c66beddc15d84c11689
ljh95 <ilegwmam@gmail.com>
2021-12-12 19:19:36 +0900

    계산 로직은 다형성을 통해 처리

--- chapter01/마지막/createStatementData.js
+++ chapter01/마지막/createStatementData.js
@@ -5,27 +5,7 @@ class PerformanceCalculator { // 공연료 계산기 클래스
   }
 
   get amount() {
-    let result = 0; 
-  
-    switch (this.play.type) {
-      case "tragedy":
-        result = 40000;
-        if(this.performance.audience > 30) {
-          result += 1000 * (this.performance.audience - 30);
-        }
-        break;
-      case "comedy":
-        result = 30000;
-        if(this.performance.audience > 20) {
-          result += 10000 + 500 * (this.performance.audience - 20);
-        }
-        result += 300 * this.performance.audience;
-        break;
-      default:
-        throw new Error(`알 수 없는 장르: ${this.play.type}`);
-    }
-  
-    return result;
+    throw new Error('서브클래스에서 처리하도록 설계되었습니다.')
   }
 
   get volumeCredits() {
@@ -33,6 +13,35 @@ class PerformanceCalculator { // 공연료 계산기 클래스
     result += Math.max(this.performance.audience - 30, 0);
 
     if("comedy" === this.play.type) result += Math.floor(this.performance.audience / 5);
+    return result;
+  }
+}
+
+function createPerformanceCalculator(aPerformance, aPlay) {
+  switch (aPlay.type) {
+    case 'tragedy': return new TragedyCalculator(aPerformance, aPlay);
+    case 'comedy': return new ComedyCalculator(aPerformance, aPlay);
+    default:
+      throw new Error(`알 수 없는 장르: ${aPlay.type}`);
+  }
+}
+class TragedyCalculator extends PerformanceCalculator{
+  get amount() {
+    let result = 40000;
+    if (this.performance.audience > 30) {
+      result += 1000 * (this.performance.audience - 30);
+    }
+    return result;
+  }
+}
+
+class ComedyCalculator extends PerformanceCalculator{
+  get amount() {
+    let result = 30000;
+    if(this.performance.audience > 20) {
+      result += 10000 + 500 * (this.performance.audience - 20);
+    }
+    result += 300 * this.performance.audience;
     return result;
   }
 }
@@ -46,7 +55,7 @@ export default function createStatementData(invoice, plays){
   return result;
 
   function enrichPerformance(aPerformance) {
-    const calculator = new PerformanceCalculator(aPerformance, playFor(aPerformance));
+    const calculator = createPerformanceCalculator(aPerformance, playFor(aPerformance));
 
     const result = Object.assign({}, aPerformance); // 얕은 복사
     result.play = calculator.play; // 중간 데이터에 연극정보를 저장
