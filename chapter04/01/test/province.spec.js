d29a3542c903e9b4525395d7e208c98242d48ac2
ljh95 <ilegwmam@gmail.com>
2021-12-23 20:27:28 +0900

    add: assert -> expect

--- chapter04/01/test/province.spec.js
+++ chapter04/01/test/province.spec.js
@@ -1,9 +1,11 @@
 import { Province, sampleProvinceData } from '../province.js';
 import assert from 'node:assert'
+import expect from 'expect';
 
 describe('province', function() {
   it('shorfall', function() {
     const asia = new Province(sampleProvinceData()); // 픽스처 설명
-    assert.equal(asia.shortfall, 5); // 검증
+    // assert.equal(asia.shortfall, 5); // 검증
+    expect(asia.shortfall).toEqual(5)
   })
 })
